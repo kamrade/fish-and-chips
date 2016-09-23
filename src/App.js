7c944ad37133e7fd53cc20965e2dbfaf541f44d0
Denis Michailov <denis.michailov@gmail.com>
2016-09-24 00:38:31 +1100

    sync with firebase

--- src/App.js
+++ src/App.js
@@ -6,6 +6,16 @@ import Order from './components/order';
 import Inventory from './components/inventory';
 import Fish from './components/fish';
 
+// FIREBASE
+import Rebase from 're-base';
+var base = Rebase.createClass({
+    apiKey: "AIzaSyAQl29WXbRa26nIU5KLaAl7zZPm4T_ar58",
+    authDomain: "fish-n-chips.firebaseapp.com",
+    databaseURL: "https://fish-n-chips.firebaseio.com",
+    storageBucket: "fish-n-chips.appspot.com",
+    messagingSenderId: "87263486017"
+});
+// END OF FIREBASE
 
 var App = React.createClass({
     getInitialState: function(){
@@ -13,6 +23,14 @@ var App = React.createClass({
             fishes: {},
             order: {}
         }
+    },
+    // Firebase
+    componentDidMount: function(){
+        base.syncState(this.props.params.storeId + '/fishes', {
+            context: this,
+            state: 'fishes'
+        });
+
     },
     addFish: function(newFish){
         var timeStamp = (new Date()).getTime();
